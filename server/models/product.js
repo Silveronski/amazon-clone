72ce5601e1cfe8321226a8fcf55433b17e7dcd0e
Silveronski <153466946+Silveronski@users.noreply.github.com>
2024-06-21 17:00:42 +0300

    initialize client and finish models in server

---
+++ server/models/product.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true
+    },   
+    category: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    countInStock: {
+        type: Number,
+        required: true
+    },
+    token: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    brand: {
+        type: String,
+        required: true
+    },
+    rating: {
+        rate: { type: Number },
+        count: { type: Number }
+    }
+}, {timestamps: true});
+
+module.exports = mongoose.model('Product', productSchema);
